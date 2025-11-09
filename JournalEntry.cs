bef597f4ebca5581b1e16668bc4ede0d6c67f940
etienne <etienne_tessier100@hotmail.ca>
2025-11-09 12:55:29 -0500

    Add project files.

---
+++ JournalEntry.cs
@@ -0,0 +1,8 @@
+namespace api_mood_journal
+{
+    public class JournalEntry
+    {
+        public DateOnly Date { get; set; }
+
+    }
+}
---
+++ WeatherForecast.cs
@@ -0,0 +1,8 @@
+namespace api_mood_journal
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+    }
+}
